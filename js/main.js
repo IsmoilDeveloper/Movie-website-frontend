8ffe77331718dfaf908b82dcedf1b9187ed4c267
Ismoil <72316546+IsmoilDeveloper@users.noreply.github.com>
2020-10-21 02:35:02 +0500

    full movie website but not responsive
    This website is included grid,flexbox and before after in css.
    Additionally slick and flexbox are new thing to understand where to make slides, fading out,in and how to link to YOUTUBE easely.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>'
+  });
+  
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-img',
+    fade: true,
+    arrows: false
+  });
+
+});
